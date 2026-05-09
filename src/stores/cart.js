1378c0189c711cb607a3c394de6f6f57a244547e
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-09 03:33:23 -0400

    initial commit

---
+++ src/stores/cart.js
@@ -0,0 +1,16 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', () => {
+  const items = ref([])
+
+  function addItem(product) {
+    items.value.push({ ...product, quantity: 1 })
+  }
+
+  function removeItem(productId) {
+    items.value = items.value.filter(item => item.id !== productId)
+  }
+
+  return { items }
+})
