d1aa5d5dfde8e05dec7cbf502ad2a50349fd32e1
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-09 14:19:07 -0400

    testing branch protections

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -4,13 +4,10 @@ import { defineStore } from 'pinia'
 export const useCartStore = defineStore('cart', () => {
   const items = ref([])
 
-  function addItem(product) {
-    items.value.push({ ...product, quantity: 1 })
-  }
 
   function removeItem(productId) {
     items.value = items.value.filter((item) => item.id !== productId)
   }
 
-  return { items, addItem, removeItem }
+  return { items, removeItem }
 })
