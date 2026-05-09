6e45571f8fd3b5784db9859e5c593ad72571a2be
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-09 14:21:18 -0400

    should throw a red flag and block merge

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -4,10 +4,9 @@ import { defineStore } from 'pinia'
 export const useCartStore = defineStore('cart', () => {
   const items = ref([])
 
-
   function removeItem(productId) {
     items.value = items.value.filter((item) => item.id !== productId)
   }
 
-  return { items, removeItem }
+  return { items }
 })
