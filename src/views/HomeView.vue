cdcd64ff7b1577793de37ae36a80c7692de1741a
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-10 01:43:45 -0400

    configured bootstrap/cleanup default vue code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Products from '@/components/ProductsContainer.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Products />
   </main>
 </template>
