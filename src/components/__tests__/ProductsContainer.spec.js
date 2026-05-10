78e1f72f0ca5f64e359aa8e2786985db80ec5883
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-10 01:55:37 -0400

    check for github workers running e2e tests before merge

--- src/components/__tests__/ProductsContainer.spec.js
+++ src/components/__tests__/ProductsContainer.spec.js
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import ProductsContainer from '../ProductsContainer.vue'
 
-describe('HelloWorld', () => {
+describe('ProductsContainer', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(ProductsContainer)
+    expect(wrapper.text()).toContain('products container')
   })
 })
