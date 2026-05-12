14d033ccb662ed5d2ccd9cc9c02365f96601b2cc
Jarrian McLean <jarrian.mclean@gmail.com>
2026-05-11 21:53:50 -0400

    add modal popup component for product listings:

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,20 +1,28 @@
 <script setup>
+import { ref } from 'vue'
+import ProductModal from '@/components/ProductModal.vue'
+
 defineProps({
   product: {
     type: Object,
     required: true,
   },
 })
+
+const open = ref(false)
+const toggleModal = () => {
+  open.value = !open.value
+}
 </script>
 
 <template>
   <div class="col">
-    <div class="card">
+    <div class="card" data-bs-theme="dark" @click="toggleModal">
       <img :src="product.images[0]" class="card-img-top" :alt="product.title" />
       <div class="card-body">
         <h5 class="card-title">{{ product.title }}</h5>
-        <p class="card-text">{{ product.description }}</p>
       </div>
     </div>
   </div>
+  <ProductModal :product="product" :open="open" @close="toggleModal" />
 </template>
